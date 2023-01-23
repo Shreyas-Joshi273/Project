8ffaf4a094f669f3ffd5e0eae807530cf6013a44
SHREYAS JOSHI <120446956+Shreyas-Joshi273@users.noreply.github.com>
2023-01-23 09:39:58 +0530

    All the exercise of Html, Css, Js added

---
+++ Part-2/FAQ-Accordion-Card -1/faq-accordion-master/script.js
@@ -0,0 +1,11 @@
+// Let's Create A JavaScript File
+
+const questions = document.querySelectorAll(".question")
+questions.forEach((question) => question.addEventListener("click", () => {
+    if (question.parentNode.classList.contains("active")) {
+        question.parentNode.classList.toggle("active")
+    } else {
+        questions.forEach(question => question.parentNode.classList.remove("active"))
+        question.parentNode.classList.add("active")
+    }
+}));
